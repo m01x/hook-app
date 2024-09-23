a4634e2bc5e716bf32abbd9774b1730d0bdc332a
m01x <fortiz.informatica@gmail.com>
2024-09-23 00:31:32 -0300

    feat(01-useState Hook): Se agrega funcionalidad al contador de ejemplo con useState, se empieza a trabajar con el custom hook

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,14 +1,75 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const CounterApp = () => {
+  
+  const [state,setCounter] = useState({
+    counter1:10,
+    counter2:20,
+    counter3:30,
+  });
 
-const [counter, setCounter] = useState(10)
+  const { counter1, counter2, counter3 } = state;
 
-  return (
+  return(
     <>
-    <h1>Counter: {counter}</h1>
+    <h1>Counter1: { counter1 }</h1>
+    <h1>Counter2: { counter2 }</h1>
+    <h1>Counter3: { counter3 }</h1>
+
     <hr />
-    <button className="btn" onClick={()=> setCounter(counter+1)}>+1</button>
+
+    <button 
+      className="btn"
+      onClick={
+        () => setCounter({
+          ...state,
+          counter1: counter1 + 1
+        })
+      }
+    >+1</button>
     </>
   )
 }
+
+
+
+
+
+
+
+
+
+/**
+ * Old way
+ */
+
+// import { useState } from "react"
+
+// export const CounterApp = () => {
+
+// const [counter, setCounter] = useState({
+//   counter1:10,
+//   counter2:20,
+//   counter3:30,
+// })
+
+// const handleCounter = () => {
+//   const estado = {
+//     counter1:counter.counter1 + 1,
+//     counter2:counter.counter2,
+//     counter3:counter.counter3,
+//   }
+
+//   setCounter(estado)
+// }
+
+//   return (
+//     <>
+//     <h1>Counter1: {counter.counter1}</h1>
+//     <h1>Counter2: {counter.counter2}</h1>
+//     <h1>Counter3: {counter.counter3}</h1>
+//     <hr />
+//     <button className="btn" onClick={()=> handleCounter()}>+1</button>
+//     </>
+//   )
+// }
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 //import { HooksApp } from './HooksApp.jsx'
 import { CounterApp } from './01-useState/CounterApp'
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </StrictMode>
 )
 
