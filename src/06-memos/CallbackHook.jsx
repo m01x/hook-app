437eba7d09add00425fd7c8346757ad69aaff349
m01x <fortiz.informatica@gmail.com>
2024-09-29 21:14:15 -0300

    feat(Tarea - memo): Se agregan funcionalidades a los hooks de memo y se agrega la tarea finalizada.

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -8,8 +8,8 @@ export const CallbackHook = () => {
     const [counter, setCounter] = useState(10);
 
     const incrementFather = useCallback(
-      () => {
-        setCounter(  (value) => value + 1 );
+      (value) => {
+        setCounter(  (c) => c + value );
       },
       [],
     )
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 // import { MemoHook } from './06-memos/MemoHook'
 //import { FocusScreen } from './04-useRef/FocusScreen'
 //import { Memorize } from './06-memos/Memorize'
@@ -11,10 +11,11 @@ import { CallbackHook } from './06-memos/CallbackHook'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+//import { CallbackHook } from './06-memos/CallbackHook'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <CallbackHook />
+    <Padre />
   // </StrictMode>
 )
 
