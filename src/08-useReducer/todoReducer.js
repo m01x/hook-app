1fc66877f47be5f63d0c7a1f82800edfa2859945
m01x <fortiz.informatica@gmail.com>
2024-10-03 23:49:29 -0300

    feat(TodoApp): Se implementa el reducer, con el localstorage para persistir datos.

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,8 +1,8 @@
 export const todoReducer = ( initialState =[], action) => {
   
     switch (action.type) {
-        case 'ABC':
-            throw new Error('Action.type no está implementada.');
+        case '[TODO] Add Todo':
+            return [ ...initialState, action.payload ];
                 
         default:
             return initialState;
