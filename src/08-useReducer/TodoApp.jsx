29c4c9159cb41f301408fb315bcc6ad84058b9f9
m01x <fortiz.informatica@gmail.com>
2024-10-04 23:12:44 -0300

    feat(TodoApp): Se agrega el handleToggleTodo y handleDeleteTodo

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -27,20 +27,41 @@ export const TodoApp = () => {
       dispatch( action );
     }
 
+    const handleDeleteTodo = ( id ) => {
+      //una forma corta
+      dispatch({
+
+        type:'[TODO] Remove Todo',
+        payload: id
+
+      });
+    }
+
+    const handleToggleTodo = ( id ) => {
+      dispatch({
+        type:'[TODO] Remove Todo',
+        payload: id
+
+      });
+    }
+
   return (
     <>
         <h1>TodoApp: 10 , <small>pendientes: 2</small></h1>
         <hr />
 
         <div className='row'>
+
             <div className='col-7'>
-                <TodoList todos={todos}/>           
+                <TodoList todos={todos} onDeleteTodo={ handleDeleteTodo } onToggleTodo={ handleToggleTodo }/>           
             </div>
+
             <div className="col-5">
                 <h4>Agregar TODO</h4>
                 <hr />
                 <TodoAdd handleNewTodo={handleNewTodo} />
             </div>
+
         </div>
 
     </>
